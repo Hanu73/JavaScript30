ccc5e1b1407fc7d0de9d17ddadd01d9ba1cd1d41
hanu73 <saihanuman73@gmail.com>
2022-09-09 23:58:12 +0530

    JavaScript -11 : Custom Video Player

---
+++ custom-video-player/script.js
@@ -0,0 +1,52 @@
+const player = document.querySelector(".player");
+const video = document.querySelector(".viewer");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+progressBar.style.flexBasis = `1%`;
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updateButton() {
+  toggle.textContent = this.paused ? "►" : "❚ ❚";
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRanges() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+skipButtons.forEach((button) => {
+  button.addEventListener("click", skip);
+});
+ranges.forEach((range) => range.addEventListener("change", handleRanges));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+
+// need to make touch events too , so that we can access in mobile
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
